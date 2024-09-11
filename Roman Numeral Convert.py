68bd566f95be3d420823a73264832361fcf5d9cf
12pounce89 <astorrs27@gmail.com>
2024-09-11 14:46:58 -0400

    update

---
+++ Roman Numeral Convert.py
@@ -0,0 +1,27 @@
+number = 0
+number_list = []
+reversed = []
+
+def convert_thousands():
+    f
+
+def convert_hundreds():
+    f
+
+def convert_tens():
+    f
+
+def convert_ones():
+    f
+
+def main():
+    global number
+    global number_list
+    global reversed
+    number = input
+    number_list = list(number)
+    for i in range (len(number_list) - 1, 0, -1):
+        reversed.append(number_list[i])
+    
+
+main()
