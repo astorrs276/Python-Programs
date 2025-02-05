de0149ed67883614f80ac6c96864e25daec022e0
12pounce89 <astorrs27@gmail.com>
2025-02-05 12:08:11 -0500

    2/5

---
+++ Leetle Programs/Single Linked List Middle.py
@@ -0,0 +1,16 @@
+# Singly Linked List Definition
+# class ListNode:
+#         def __init__(self, val=0, next=None):
+#                 self.val = val
+#                 self.next = next
+
+def solve(head):
+    node = head
+    count = 1
+    while node.next is not None:
+        count += 1
+        node = node.next
+    node = head
+    for i in range(count // 2):
+        node = node.next
+    return node
