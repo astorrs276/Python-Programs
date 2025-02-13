93cb2b0639a01cac0bdd3d6496b666aa297a7a63
12pounce89 <astorrs27@gmail.com>
2025-02-13 11:53:55 -0500

    circular primes failed

---
+++ Project Euler/Circular Primes.py
@@ -0,0 +1,32 @@
+import csv
+
+count = 0
+primes = []
+circular = []
+
+with open("data/Primes Below 1 Million.csv") as file:
+    csvReader = csv.reader(file)
+    for line in csvReader:
+        primes.extend(line)
+
+print(primes)
+print(primes[1], type(primes[1]))
+
+for j, prime in enumerate(primes):
+    if prime in circular:
+        continue
+    listed = list(str(prime))
+    options = []
+    for i in range(len(listed)):
+        if int("".join(listed)) not in primes:
+            break
+        listed.append(listed.pop(0))
+        options.append("".join(listed))
+    else:
+        count += 1
+        circular.extend(options)
+    if j % 1000 == 0:
+        print(j)
+
+print(len(primes))
+print(count)
