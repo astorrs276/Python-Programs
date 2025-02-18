11bd6ed131d6e99b67d86cfe1cf0a8d7675c14ca
12pounce89 <astorrs27@gmail.com>
2025-02-18 11:42:27 -0500

    2/18

---
+++ Leetle Programs/Remove Nth Node From End of List.py
@@ -0,0 +1,18 @@
+# class ListNode:
+#         def __init__(self, val=0, next=None):
+#                 self.val = val
+#                 self.next = next
+
+def solve(head, n):
+    dummy = ListNode(0, head)
+    first = second = dummy
+        
+    for _ in range(n + 1):
+        first = first.next
+        
+    while first:
+        first, second = first.next, second.next
+        
+    second.next = second.next.next
+        
+    return dummy.next
