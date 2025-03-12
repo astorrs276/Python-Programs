d053076e13e04bc772493b3037e6200cc0839d76
Alex Storrs <astorrs27@gmail.com>
2025-03-12 14:53:29 -0400

    more flags

--- CTF Stuff/Base64 Decoder.py
+++ CTF Stuff/Base64 Decoder.py
@@ -1,7 +1,9 @@
 import base64
 
 values = ["cGljb0NURntjMDBrMWVfbTBuc3Rlcl9sMHZlc19jMDBraWVzX0FDOEZDRDc1fQ==", # picoCTF - Cookie Monster Secret Recipe
-            "cGljb0NURnt3ZWJfc3VjYzNzc2Z1bGx5X2QzYzBkZWRfMTBmOTM3NmZ9" # picoCTF - WebDecode
+            "cGljb0NURnt3ZWJfc3VjYzNzc2Z1bGx5X2QzYzBkZWRfMTBmOTM3NmZ9", # picoCTF - WebDecode
+            "YidkM0JxZGtwQlRYdHFhR3g2YUhsZmF6TnFlVGwzWVROclgya3lNRFJvYTJvMmZRPT0nCg==", # picoCTF - interencdec p1
+            "d3BqdkpBTXtqaGx6aHlfazNqeTl3YTNrX2kyMDRoa2o2fQ==", # picoCTF - interencdec p2
             ]
 
 value = values[-1]
