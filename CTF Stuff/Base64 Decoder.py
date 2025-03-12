5a08504ccdcaf7bf7352c15ed2c52f87e8ae2c11
Alex Storrs <astorrs27@gmail.com>
2025-03-12 14:27:32 -0400

    base64

--- CTF Stuff/Base64 Decoder.py
+++ CTF Stuff/Base64 Decoder.py
@@ -1,6 +1,10 @@
 import base64
 
-value = "cGljb0NURntjMDBrMWVfbTBuc3Rlcl9sMHZlc19jMDBraWVzX0FDOEZDRDc1fQ=="
+values = ["cGljb0NURntjMDBrMWVfbTBuc3Rlcl9sMHZlc19jMDBraWVzX0FDOEZDRDc1fQ==", # picoCTF - Cookie Monster Secret Recipe
+            "cGljb0NURnt3ZWJfc3VjYzNzc2Z1bGx5X2QzYzBkZWRfMTBmOTM3NmZ9" # picoCTF - WebDecode
+            ]
+
+value = values[-1]
 
 decoded_flag = base64.b64decode(value).decode()
 
