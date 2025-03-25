608a7827a877975b02a62436ec60e4d3e68a0a23
12pounce89 <astorrs27@gmail.com>
2025-03-25 12:57:58 -0400

    puzzle 1 complete

--- Internationalization Puzzles/Puzzle 1 - Length Limits on Messaging Platforms.py
+++ Internationalization Puzzles/Puzzle 1 - Length Limits on Messaging Platforms.py
@@ -5,10 +5,12 @@ def check_characters(string):
     return len(string) <= 140
 
 cost = 0
-with open("Internationalization Puzzles/data/test.txt") as file:
+
+with open("Internationalization Puzzles/data/puzzle1.txt", "r", encoding="utf-8") as file:
     for line in file:
-        sms_safe = check_bytes(line)
-        tweet_safe = check_characters(line)
+        stripped_line = line.strip()
+        sms_safe = check_bytes(stripped_line)
+        tweet_safe = check_characters(stripped_line)
         if sms_safe and tweet_safe:
             cost += 13
         elif sms_safe and not tweet_safe:
