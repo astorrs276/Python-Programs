eb0e9bd110239eda71e7dc1fb3fae18cb82a47c9
12pounce89 <astorrs27@gmail.com>
2025-04-11 15:17:31 -0400

    4/10-4/11

---
+++ Leetle Programs/Reverse Words in a String.py
@@ -0,0 +1,2 @@
+def solve(s):
+    return " ".join(s.split(" ")[::-1])
---
+++ Leetle Programs/Rotate Linked List.py
@@ -0,0 +1,20 @@
+# class ListNode:
+#         def __init__(self, val=0, next=None):
+#                 self.val = val
+#                 self.next = next
+
+def solve(head, k):
+    if (head is None):
+        return head
+    elif (head.next is None):
+        return head
+    for i in range(k):
+        last = None
+        node = head
+        while(node.next != None):
+            last = node
+            node = node.next
+        new_node = ListNode(node.val, head)
+        last.next = None
+        head = new_node
+    return head
