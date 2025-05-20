7a7ed6350f0d7642681e990d512e8391219b2bce
Alex Storrs <astorrs27@gmail.com>
2025-05-20 11:32:26 -0400

    algorithm

--- CTF Stuff/Algorithms.py
+++ CTF Stuff/Algorithms.py
@@ -12,3 +12,17 @@ def extended_gcd(a, b):
         y = x1 - (a // b) * y1
         return gcd, x, y
 
+def prime_factors(n):
+    factors = []
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            factors.append(d)
+            n //= d
+        d += 1
+        if d * d > n:
+            if n > 1:
+                factors.append(n)
+            break
+    return factors
+
